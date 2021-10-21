31954c23648675fb0abf608e11b99618c8e65923
Basila123 <basilanoushad92@gmail.com>
2021-10-21 09:39:21 +0530

    Welcome to Tic Tac

--- Tic_new/src/com/bridgelabz_TicTac/UC1.java
+++ Tic_new/src/com/bridgelabz_TicTac/UC1.java
@@ -5,16 +5,13 @@ public class UC1 {
     public static void main(String arg[]) {
 
 
-        int a[]= new int[10];
+        char board[]= new char[10];
         for (int i=1;i<10;i++){
-            
+
+            board[i]=' ';
         }
-        int board=0;
-        for (int i=1;i<=10;i++)
-        {
-            System.out.println(a[i]);
-            board=a[i];
-        }
+
+
     }
 
 }
