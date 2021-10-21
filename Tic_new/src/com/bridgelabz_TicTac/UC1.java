d4d939b37572f0a7fc5e679f3b935de569532b18
Basila123 <basilanoushad92@gmail.com>
2021-10-21 09:26:43 +0530

    I tried to push

---
+++ Tic_new/src/com/bridgelabz_TicTac/UC1.java
@@ -0,0 +1,20 @@
+package com.bridgelabz_TicTac;
+
+public class UC1 {
+
+    public static void main(String arg[]) {
+
+
+        int a[]= new int[10];
+        for (int i=1;i<10;i++){
+            
+        }
+        int board=0;
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(a[i]);
+            board=a[i];
+        }
+    }
+
+}
